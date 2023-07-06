e048e47a071299c84308f48fa1955ab9dd78dcc6
santheartist <vazquezbare@gmail.com>
2023-07-06 17:02:14 -0300

    dashboard features

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // import children components
-import GenericOrgPost from '../components/GenericOrgPost';
+import GenericOrgPost from "../components/GenericOrgPost";
 
 // import all social dbs
 // import TikTok from "../data/TikTok";
@@ -12,140 +12,180 @@ import GenericOrgPost from '../components/GenericOrgPost';
 // console.log('H E R E 0- - - - -', allPosts);
 
 const Dashboard = () => {
+  // Fetch DB JSON
+  const [contents, setContents] = useState([]);
+  const [filteredContents, setFilteredContents] = useState([]);
 
-   // Fetch DB JSON
-   const [contents, setContents] = useState([]);
-   const [filteredContents, setFilteredContents] = useState([]);
-
-   useEffect(() => {
+  useEffect(() => {
     const fetchContents = async () => {
       try {
-        const response = await fetch('http://localhost:3500/content'); // Replace with the actual API route for fetching users
+        const response = await fetch("http://localhost:3500/content"); // Replace with the actual API route for fetching users
         if (response.ok) {
           const data = await response.json();
           setContents(data);
-          setFilteredContents(data);  // Initially, all users are shown
+          setFilteredContents(data); // Initially, all users are shown
         } else {
-          console.error('Failed to fetch content');
+          console.error("Failed to fetch content");
         }
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
 
     fetchContents();
   }, []);
 
-    // Filters
-    const [socialFilter, setSocialFilter] = useState(null);
-    const [dateFilter, setDateFilter] = useState(null);
-    const [typeFilter, setTypeFilter] = useState(null);
-    const [textFilter, setTextFilter] = useState('');
+  // Filters
+  const [socialFilter, setSocialFilter] = useState(null);
+  const [dateFilter, setDateFilter] = useState(null);
+  const [typeFilter, setTypeFilter] = useState(null);
+  const [textFilter, setTextFilter] = useState("");
 
-    useEffect(() => {
-      const filterContents = () => {
-        const newFilteredContents = contents.filter(content => {
-          return  (socialFilter ? content.platform.includes(socialFilter) : true) && 
-                  (dateFilter ? (new Date(content.postDate) >= convertDates(dateFilter).startDate && new Date(content.postDate) <= convertDates(dateFilter).endDate) : true) &&
-                  (typeFilter ? content.type === typeFilter : true) &&
-                  (textFilter ? content.description === textFilter : true)
-        });
-        setFilteredContents(newFilteredContents);
-      };
-      
+  useEffect(() => {
+    const filterContents = () => {
+      const newFilteredContents = contents.filter((content) => {
+        return (
+          (socialFilter ? content.platform.includes(socialFilter) : true) &&
+          (dateFilter
+            ? new Date(content.postDate) >=
+                convertDates(dateFilter).startDate &&
+              new Date(content.postDate) <= convertDates(dateFilter).endDate
+            : true) &&
+          (typeFilter ? content.type === typeFilter : true) &&
+          (textFilter
+            ? Object.values(content).some(
+                (value) =>
+                  typeof value === "string" &&
+                  value.toLowerCase().includes(textFilter.toLowerCase())
+              )
+            : true)
+        );
+      });
+      setFilteredContents(newFilteredContents);
+    };
     filterContents();
-  }, [socialFilter, dateFilter, typeFilter, contents]);
+  }, [socialFilter, dateFilter, typeFilter, textFilter, contents]);
 
-    // Filters
-    const handleSocialFilterChange = (event) => {
-        setSocialFilter(event.target.value);
-      };
-    const handleDateFilterChange = (event) => {
-        setDateFilter(event.target.value);
-      };
-    const handleTypeFilterChange = (event) => {
-        setTypeFilter(event.target.value);
-      };
-    const handleTextFilterChange = (event) => {
-        setTextFilter(event.target.value);
-      };
+  // Filters
+  const handleSocialFilterChange = (event) => {
+    setSocialFilter(event.target.value);
+  };
+  const handleDateFilterChange = (event) => {
+    setDateFilter(event.target.value);
+  };
+  const handleTypeFilterChange = (event) => {
+    setTypeFilter(event.target.value);
+  };
+  const handleTextFilterChange = (event) => {
+    setTextFilter(event.target.value);
+  };
 
-    // Convert select options to actual date filters
-    function convertDates(filter) {
-      const today = new Date();
-      let startDate, endDate;
-  
-      switch(filter) {
-      case 'today':
-          startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-          endDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59);
-          break;
-      case 'this_week':
-          const firstDayOfWeek = today.getDate() - today.getDay();
-          startDate = new Date(today.getFullYear(), today.getMonth(), firstDayOfWeek);
-          endDate = new Date(today.getFullYear(), today.getMonth(), firstDayOfWeek + 7);
-          break;
-      case 'this_month':
-          startDate = new Date(today.getFullYear(), today.getMonth(), 1);
-          endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-          break;
+  // Convert select options to actual date filters
+  function convertDates(filter) {
+    const today = new Date();
+    let startDate, endDate;
+
+    switch (filter) {
+      case "today":
+        startDate = new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          today.getDate()
+        );
+        endDate = new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          today.getDate(),
+          23,
+          59,
+          59
+        );
+        break;
+      case "this_week":
+        const firstDayOfWeek = today.getDate() - today.getDay();
+        startDate = new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          firstDayOfWeek
+        );
+        endDate = new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          firstDayOfWeek + 7
+        );
+        break;
+      case "this_month":
+        startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        break;
       default:
-          startDate = null;
-          endDate = null;
-      }
-  
-      return { startDate, endDate };
+        startDate = null;
+        endDate = null;
+    }
+
+    return { startDate, endDate };
   }
 
   return (
-    <div className='dashboard-wrapper'>
+    <div className="dashboard-wrapper">
+      {/* filters */}
+      <aside className="filters">
+        <h3>Filters</h3>
+        <div className="filters-wrapper">
+          <div className="filter-container">
+            <label>
+              <b>Platform:</b>{" "}
+            </label>
+            <select onChange={handleSocialFilterChange}>
+              <option value="">All</option>
+              <option value="Instagram">IG</option>
+              <option value="TikTok">TikTok</option>
+              <option value="Pinterest">Pinterest</option>
+              <option value="Facebook">Facebook</option>
+              <option value="Twitter">Twitter</option>
+            </select>
+          </div>
+          <div className="filter-container">
+            <label>
+              <b>Date:</b>{" "}
+            </label>
+            <select onChange={handleDateFilterChange}>
+              <option value="">All</option>
+              <option value="today">Today</option>
+              <option value="this_week">This Week</option>
+              <option value="this_month">This Month</option>
+            </select>
+          </div>
+          <div className="filter-container">
+            <label>
+              {" "}
+              <b>Type:</b>{" "}
+            </label>
+            <select onChange={handleTypeFilterChange}>
+              <option value="">All</option>
+              <option value="post">Post</option>
+              <option value="story">Story</option>
+            </select>
+          </div>
+          <div className="filter-container">
+            <label>
+              <b>Words:</b>
+            </label>
+            <input
+              type="text"
+              onChange={handleTextFilterChange}
+              placeholder="Type any word"
+            />
+          </div>
+        </div>
+      </aside>
 
-        {/* filters */}
-        <aside className="filters">
-            <h3>Filters</h3>
-            <div className="filters-wrapper">
-                <div className='filter-container'>
-                  <label><b>Platform:</b> </label>
-                  <select onChange={handleSocialFilterChange}>
-                    <option value="">All</option>
-                    <option value="Instagram">IG</option>
-                    <option value="TikTok">TikTok</option>
-                    <option value="Pinterest">Pinterest</option>
-                    <option value="Facebook">Facebook</option>
-                    <option value="Twitter">Twitter</option>
-                  </select>
-                </div>
-                <div className='filter-container'>
-                  <label><b>Date:</b> </label>
-                  <select onChange={handleDateFilterChange}>
-                    <option value="">All</option>
-                    <option value="today">Today</option>
-                    <option value="this_week">This Week</option>
-                    <option value="this_month">This Month</option>
-                  </select>
-                </div>
-                <div className='filter-container'>
-                  <label> <b>Type:</b> </label>
-                  <select onChange={handleTypeFilterChange}>
-                    <option value="">All</option>
-                    <option value="post">Post</option>
-                    <option value="story">Story</option>
-                  </select>
-                </div>
-                <div className='filter-container'>
-                  <label><b>Words:</b></label>
-                  <input type="text" onChange={handleTextFilterChange} placeholder='Type any word'/>
-                </div>
-            </div>
-          </aside>
+      <section>
+        <h2 style={{ textAlign: "center", margin: "40px 0" }}>Dashboard</h2>
 
-          <section>
-            <h2 style={{textAlign:'center', margin:'40px 0'}}>Dashboard</h2>
-
-            <h2> Content </h2>
-            <div className='dashboard-posts-section'>
-            
-            {/* {
+        <h2> Content </h2>
+        <div className="dashboard-posts-section">
+          {/* {
               contents
                 ? contents
                     .filter(item => {
@@ -191,29 +231,26 @@ const Dashboard = () => {
                   })
             } */}
 
-              {filteredContents.map((item, index) => {
-                return (
-                  <GenericOrgPost 
-                    title={item.title}
-                    caption={item.caption}
-                    description={item.description}
-                    type={item.type}
-                    postDate={item.postDate}
-                    brand={item.brand}
-                    link={item.mediaURL} 
-                    likes={item.likes} 
-                    audio={item.audio}
-                    key={index} 
-                  />
-                );
-              })}
-
-            
-            
-                    </div>
-          </section>
+          {filteredContents.map((item, index) => {
+            return (
+              <GenericOrgPost
+                title={item.title}
+                caption={item.caption}
+                description={item.description}
+                type={item.type}
+                postDate={item.postDate}
+                brand={item.brand}
+                link={item.mediaURL}
+                likes={item.likes}
+                audio={item.audio}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
--- database/db-creator.sql
+++ database/db-creator.sql
@@ -41,14 +41,6 @@ CREATE TABLE `content` (
    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
 
--- CREATE TABLE `scripts` (
---    `id` INT NOT NULL AUTO_INCREMENT,
---    -- add columns for the 'scripts' table here
---    PRIMARY KEY (`id`)
--- );
-
--- ALTER TABLE `content` ADD FOREIGN KEY (`script_id`) REFERENCES `scripts`(`id`);
-
 CREATE TABLE `content_hashtags` (
    `content_id` INT,
    `hashtag_id` INT,
@@ -62,3 +54,45 @@ CREATE TABLE `content_tagged_users` (
    FOREIGN KEY (`content_id`) REFERENCES `content`(`id`),
    FOREIGN KEY (`tagged_user_id`) REFERENCES `tagged_users`(`id`)
 );
+
+-- Creating users
+INSERT INTO `user` (`name`, `username`, `password`, `status`) VALUES
+   ('Master User', 'john', '123', 'master'),
+   ('User User', 'jane', '123', 'user'),
+   ('Client User', 'client', '123', 'client');
+
+-- Creating content entries
+INSERT INTO `content` (`user_id`, `platform`, `title`, `caption`, `type`, `timestamp`)
+VALUES
+   -- TikTok entries
+   (1, 'TikTok', 'TikTok Entry 1', 'Check out my TikTok video!', 'video', NOW()),
+   (2, 'TikTok', 'TikTok Entry 2', 'Another TikTok video!', 'video', NOW()),
+   -- Instagram entries
+   (1, 'Instagram', 'Instagram Post', 'This is my awesome post!', 'photo', NOW()),
+   (1, 'Instagram', 'Instagram Story', 'Having a great day!', 'story', NOW()),
+   -- Pinterest entries
+   (2, 'Pinterest', 'Pinterest Entry 1', 'Discover new ideas!', 'pin', NOW()),
+   (2, 'Pinterest', 'Pinterest Entry 2', 'Pin it!', 'pin', NOW()),
+   -- Facebook entries
+   (3, 'Facebook', 'Facebook Post', 'Exciting news!', 'post', NOW()),
+   (3, 'Facebook', 'Facebook Story', 'Check out my story!', 'story', NOW());
+
+-- Creating hashtags for Instagram post
+INSERT INTO `hashtags` (`name`) VALUES
+   ('hashtag1'),
+   ('hashtag2'),
+   ('hashtag3');
+
+-- Creating content-hashtags relationship for Instagram post
+INSERT INTO `content_hashtags` (`content_id`, `hashtag_id`)
+VALUES
+   (3, 1),
+   (3, 2),
+   (3, 3);
+
+SELECT *
+FROM `content`
+WHERE `platform` = 'Instagram';
+
+SELECT DISTINCT `type`
+FROM `content`;
